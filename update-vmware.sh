97fba1bd6fcca2ba5e13e8b669e8c9d0dded2cbb
Jeremi Albrizio <jalbrizio@gmail.com>
2015-06-25 14:42:28 -0700

    changed PVSCSI_CONFED and VMMEMCTL_CONFED in etc/vmware-tools/locations to ensure the modules do not get loaded.

--- update-vmware.sh
+++ update-vmware.sh
@@ -14,7 +14,8 @@ cd /usr/local/src/vmware-tools/vmware-tools-distrib
 git fetch
 git reset --hard origin/master
 ***REMOVED*** run the vmware tool update
-***REMOVED***sed -i '/^answer\ VMMEMCTL_CONFED\ yes/c\answer\ VMMEMCTL_CONFED\ no' /etc/vmware-tools/locations
+sed -i '/^answer\ VMMEMCTL_CONFED\ yes/c\answer\ VMMEMCTL_CONFED\ no' /etc/vmware-tools/locations
+sed -i '/^answer\ PVSCSI_CONFED\ yes/c\answer\ PVSCSI_CONFED\ no' /etc/vmware-tools/locations
 /usr/local/src/vmware-tools/vmware-tools-distrib/vmware-install.pl --default --clobber-kernel-modules=vmxnet3
 ***REMOVED*** Make sure iptables will autostart at boot and restart iptables so that thecorrect rules are in place
 ***REMOVED***
