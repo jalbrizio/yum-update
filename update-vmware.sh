7f232a0dc9f16c3a06b114385609957d008811c5
Jeremi Albrizio <jalbrizio@gmail.com>
2015-06-25 13:42:04 -0700

    removed vmmemctl due to vmware replacing it with the baloon driver

--- update-vmware.sh
+++ update-vmware.sh
@@ -14,7 +14,8 @@ cd /usr/local/src/vmware-tools/vmware-tools-distrib
 git fetch
 git reset --hard origin/master
 ***REMOVED*** run the vmware tool update
-/usr/local/src/vmware-tools/vmware-tools-distrib/vmware-install.pl --default --clobber-kernel-modules=vmxnet3,pvscsi,vmmemctl
+***REMOVED***sed -i '/^answer\ VMMEMCTL_CONFED\ yes/c\answer\ VMMEMCTL_CONFED\ no' /etc/vmware-tools/locations
+/usr/local/src/vmware-tools/vmware-tools-distrib/vmware-install.pl --default --clobber-kernel-modules=vmxnet3,pvscsi
 ***REMOVED*** Make sure iptables will autostart at boot and restart iptables so that thecorrect rules are in place
 ***REMOVED***
 ***REMOVED***
