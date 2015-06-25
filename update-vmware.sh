3b507db62766bc035d7eb2be6f30dce8ac6a7fb5
Jeremi Albrizio <jalbrizio@gmail.com>
2015-06-25 13:53:23 -0700

    remove unneeded pvscsi option

--- update-vmware.sh
+++ update-vmware.sh
@@ -15,7 +15,7 @@ git fetch
 git reset --hard origin/master
 ***REMOVED*** run the vmware tool update
 ***REMOVED***sed -i '/^answer\ VMMEMCTL_CONFED\ yes/c\answer\ VMMEMCTL_CONFED\ no' /etc/vmware-tools/locations
-/usr/local/src/vmware-tools/vmware-tools-distrib/vmware-install.pl --default --clobber-kernel-modules=vmxnet3,pvscsi
+/usr/local/src/vmware-tools/vmware-tools-distrib/vmware-install.pl --default --clobber-kernel-modules=vmxnet3
 ***REMOVED*** Make sure iptables will autostart at boot and restart iptables so that thecorrect rules are in place
 ***REMOVED***
 ***REMOVED***
